b80b7990221327fdcb206bf656a18e762201acb0
blasisback <blasisback122@gmail.com>
2022-05-29 21:28:48 -0500

    Comienzo del ciclo con Spring

---
+++ uadafa/src/main/java/idat/edu/pe/gmail/com/uadafa/UadafaApplication.java
@@ -0,0 +1,13 @@
+package idat.edu.pe.gmail.com.uadafa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UadafaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UadafaApplication.class, args);
+	}
+
+}
---
+++ uadafa/src/test/java/idat/edu/pe/gmail/com/uadafa/UadafaApplicationTests.java
@@ -0,0 +1,13 @@
+package idat.edu.pe.gmail.com.uadafa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UadafaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
